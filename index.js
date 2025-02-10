6e5e15674ae23a616a386e906e702be28a63dae3
tim <exemple@gmail.com>
2025-02-10 22:20:25 +0300

    redacted

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-'use strict'
-
-let money = prompt("ваш бюджет на месяц?")
-let time = prompt("Введите дату в формате YYYY-MM-DD")
-
-let arr = [];
-
-let me = prompt('Введите обязательную статью расходов в этом месяце')
-let sum = prompt('во сколько обойдётся')
-
-let expenses = {
-        me,
-        sum
-    }
-
+let money = prompt("ваш бюджет на месяц?", '')
+let time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses,
-    optionalExpenses: null,
-    arr,
+    expenses: {},
+    optionalExpenses: {},
+    arr:[],
     saving: false
-}
+};
 
-alert(appData.budget / 30);
+for (let i = 0; i < 2; i++) {
+    let a = prompt("введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("во сколько обойдётся?", '');
+
+    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
+        && a != '' && b != '') {
+        console.log("done");
+        appData.expenses[a] = b;
+    }
+};
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("ежедневный бюджет:" + appData.moneyPerDay);
