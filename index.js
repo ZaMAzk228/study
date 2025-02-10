f6e1b7f7670874a98bc0684da602338ef0b5ad57
tim <exemple@gmail.com>
2025-02-10 17:06:26 +0300

    redacted index

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 'use strict'
 
-var a = 5;
+let money = prompt("ваш бюджет на месяц?")
+let time = prompt("Введите дату в формате YYYY-MM-DD")
 
-let persone = {
-    name: 'Notch',
-    old: 88
+let arr = [];
+
+let me = prompt('Введите обязательную статью расходов в этом месяце')
+let sum = prompt('во сколько обойдётся')
+
+let expenses = {
+        me,
+        sum
+    }
+
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses,
+    optionalExpenses: null,
+    arr,
+    saving: false
 }
 
-console.log(a)
-
+alert(appData.budget / 30);
